4f13d12fe76467b1564c411f5516fff19d26bc87
Shelby James <shelby.james@pcc.edu>
2019-12-03 14:12:24 -0800

    finished script

--- lab9_avg.sh
+++ lab9_avg.sh
@@ -2,3 +2,17 @@
 
 sum=0
 args=$#
+
+if [ $args -lt 1 ]; then
+	echo "Please enter some numbers."
+	exit 1
+fi 
+
+while [ -n "$1" ]; do
+	sum=$(expr $sum + $1)
+	shift
+done
+
+avg=$(echo "scale=2; $sum / $args"|bc)
+
+echo "The average of your $args numbers is $avg"
